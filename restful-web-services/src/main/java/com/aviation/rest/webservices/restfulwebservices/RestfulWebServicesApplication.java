09ba3cc09cb23ed92c6ea88e6ac40d983a927ca1
jevgenija-gluskova <jevgenija.gluskova@accenture.com>
2020-03-31 14:28:29 +0300

    Add Post method/ added post method for flights

--- restful-web-services/src/main/java/com/aviation/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/aviation/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -3,10 +3,12 @@ package com.aviation.rest.webservices.restfulwebservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
+ 
 	
 	@Bean
 	public RestTemplate restTemplate() {
